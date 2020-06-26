684f7af527be611b58a5048c07e30f8b2e76b0cb
Tung <31438811+VietTungFizzy@users.noreply.github.com>
2020-06-26 22:22:41 +0700

    Updated

--- login.php
+++ login.php
@@ -7,10 +7,10 @@ $message='';
 
 if(isset($_SESSION['user_id']))
 {
-    header('location:index.php');
+    header('Location:index.php');
 }
 
-if (isset($_POST["login"]))
+if (isset($_POST['login']))
 {
     $query = "SELECT * FROM login WHERE username= :username";
     $statement = $connect->prepare($query);
@@ -20,6 +20,63 @@ if (isset($_POST["login"]))
     $count = $statement->rowCount();
     if($count > 0)
     {
-        
+        $result = $statement->fetchAll();
+        foreach ($result as $row)
+        {
+            if (password_verify($_POST["password"], $row["password"])) {
+                $_SESSION['user_id'] = $row['user_id'];
+                $_SESSION['username'] = $row['username'];
+                $sub_query = "INSERT INTO login_details (user_id) VALUES ('".$row['user_id']."')";
+                $statement = $connect->prepare($sub_query);
+                $statement->execute();
+                $_SESSION['login_details_id'] = $connect->lastInsertId();
+                header('Location:index.php');
+            }
+            else 
+            {
+                $message = "<label>Wrong Password</label>";
+            }
+        }
+    }
+    else
+    {
+        $message = "<label>Wrong Username</label>";
     }
 }
+?>
+
+<html>
+<head>
+	<title>Chat Application using PHP Ajax jQuery</title>
+	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"> 
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.js"></script>
+	<script type="text/javascript" src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+</head>
+<body>
+	<div class="container">
+		<br/>
+		<h3 align="center">Chat Application using PHP Ajax jQuery</h3><br/>
+		<br/>
+		<div class="panel panel-default">
+			<div class="panel-heading">Chat Application Login</div>
+			<div class="panel-body">
+				<form method="post">
+					<p class="text-danger"><?php echo $message; ?></p>
+					<div class="form-group">
+						<label>Enter Username</label>
+						<input type="text" name="username" class="user-control" required="required"/>
+					</div>
+					<div class="form-group">
+						<label>Enter Password</label>
+						<input type="text" name="password" class="user-control" required="required"/>
+					</div>
+					<div class="form-group">
+						<input type="submit" name="login" class="btn btn-info" value="Login"/>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
